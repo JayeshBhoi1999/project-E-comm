bc12a22cf5db4d79de74a5b5595f44059f2666ba
jayesh <jayeshbhoib7@gmail.com>
2025-08-19 19:53:17 +0530

    new start

--- project_e_comm/wsgi.py
+++ project_e_comm/wsgi.py
@@ -4,7 +4,7 @@ WSGI config for project_e_comm project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
@@ -14,5 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_e_comm.settings')
 
 application = get_wsgi_application()
-
-
