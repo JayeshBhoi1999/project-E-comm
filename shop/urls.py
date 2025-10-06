f546c955b8febff551e8cb4aba25c18c887584e3
jayesh <jayeshbhoib7@gmail.com>
2025-10-06 14:48:30 +0530

    index page changes added

--- shop/urls.py
+++ shop/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path ,include
 from . import views
 urlpatterns = [
-    path('',views.home,name="home" )
+    path('',views.home,name="index" ),
+    path('<slug:p_slug>/',views.product_page,name="product_page"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 
 
 def home(request):
-    #products = Product.objects.all()
-    #return render(request,"index.html",{'products':products})
-    return render(request,"index.html")
+    products = Product.objects.all()
+    return render(request,"index.html",{'products':products})
+    #return render(request,"index.html")
+
+
+def product_page(request,p_slug):
+    product = Product.objects.get(p_slug=p_slug)
+    return render(request,"product_page.html",{"product":product})
+
 
